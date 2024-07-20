b59cbe7ecd1c0e143a999a05ccd7c006c2a15993
Kirill Gromyko <goldgrom01@gmail.com>
2024-07-20 15:59:46 +0300

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import classNames from "classnames";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
