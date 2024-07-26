6ebc53d25e25d90255706bd5a2c920858e6f0755
Kirill Gromyko <goldgrom01@gmail.com>
2024-07-26 14:17:18 +0300

    completed

--- src/components/Modal/ModalMain.jsx
+++ src/components/Modal/ModalMain.jsx
@@ -19,9 +19,12 @@ class ModalMain extends Component {
         <Button onClick={this.toggleModal}>Open modal</Button>
         <Modal isOpen={this.state.isModalOpened}>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dignissimos fuga itaque magnam mollitia rem repudiandae vel veniam
-            vitae. Consequatur, possimus.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, illum,
+            in! Architecto assumenda, blanditiis commodi cum dolorem dolorum
+            eaque error, hic, labore repudiandae sapiente temporibus velit
+            voluptatibus? Dignissimos fugit perferendis sed similique vel!
+            Aliquid aut culpa deserunt dolorem doloribus facere hic, illum iusto
+            maiores mollitia, neque nulla porro sit vero!
           </p>
           <Button onClick={this.toggleModal}>Close</Button>
         </Modal>
