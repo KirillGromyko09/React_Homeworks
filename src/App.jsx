e785b7e70992737cdd044c4e1e82a78f1ca25532
Kirill Gromyko <goldgrom01@gmail.com>
2024-08-04 17:47:17 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,73 @@
 import { useState } from "react";
+import PostCatalog from "./components/PostCatalog";
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      posts: [],
+    };
+    this.fetchUsers();
+  }
 
-function App() {
-  return
+  async fetchUsers() {
+    const request = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await request.json();
+    const filteredData = data.map(({ name, id }) => {
+      return { name, id };
+    });
+
+    this.setState({ users: filteredData });
+  }
+
+  handleClickOnListItem = async ({ id }) => {
+    const request = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + id,
+    );
+    const data = await request.json();
+
+    const styledPosts = data.map(({ title, body }) => {
+      return {
+        content: (
+          <div>
+            <h6>{title}</h6>
+            <p>{body}</p>
+          </div>
+        ),
+        title,
+        body,
+      };
+    });
+
+    this.setState({ posts: styledPosts });
+  };
+  render() {
+    return (
+      <main className="users pt-5">
+        <Container>
+          <Row>
+            <Col xs={4}>
+              {this.state.users.length ? (
+                <PostCatalog
+                  data={this.state.users}
+                  itemToShow="name"
+                  handleClick={this.handleClickOnListItem}
+                />
+              ) : (
+                "Loading..."
+              )}
+            </Col>
+            <Col xs={8}>
+              <PostCatalog data={this.state.posts} itemToShow="content" />
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/PostCatalog/PostCatalog.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import styles from "./postCatalog.module.scss";
+import { ListGroup } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
+class PostCatalog extends Component {
+  render() {
+    const { data, itemToShow: key, handleClick } = this.props;
+    return (
+      <ListGroup>
+        {data.map((item) => {
+          return (
+            <ListGroup.Item key={uuidv4()} onClick={() => handleClick(item)}>
+              {item[key]}
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+    );
+  }
+}
+PostCatalog.propTypes = {
+  data: PropTypes.array.isRequired,
+  itemToShow: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
+};
+
+export default PostCatalog;
