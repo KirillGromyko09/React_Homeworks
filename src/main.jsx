12ea38c9945d091a57f820fff277a9ed85e76c75
Kirill Gromyko <goldgrom01@gmail.com>
2024-08-05 20:33:34 +0300

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import MarkdownEditor from "./components/MarkdownEditor.jsx";
 
+function handleChangeContent(content) {
+  console.log("Content changed");
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+  <MarkdownEditor onContentChange={handleChangeContent} />,
 );
