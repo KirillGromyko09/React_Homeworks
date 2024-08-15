dbcac9a282bc19a3d76644895a9e6136bee188e7
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-08-15 14:09:04 +0300

    completed

---
+++ src/routerConfig/routeNames.js
@@ -0,0 +1,8 @@
+const routeNames = Object.freeze({
+  home: "/",
+  posts: "posts",
+  singlePost: "posts/:postId",
+  createPost: "create-post",
+  editPost: "edit-posts/:postId",
+});
+export default routeNames;
